82e3e8556e2f0e098f92db2c046f8996ab8b36ee
Cole Doster <wcdoster@gmail.com>
2018-05-18 15:43:35 -0500

    fixed registration issue

--- src/scripts/friendList/friendList.js
+++ src/scripts/friendList/friendList.js
@@ -10,28 +10,24 @@ const displayFriendList = () => {
         <button type="submit" id="add--friend">Search Friends<buttons>
     `)
     const user = sessionStorage.getItem("userID")
-    if (user !== null) {
-        console.log(user)
-        let currentUsername
-        APIManager.getSubsetUsers(user).then(currentUser => {
-            currentUsername = currentUser[0].username
-            APIManager.getSubsetFriends(user).then(friends => {
-                console.log(friends)
-                friends.forEach(friend => {
-                    const friendsToList = friend.friendID
-                    console.log(friendsToList)
-                    APIManager.getSubsetUsers(friendsToList).then(u => {
-                        u.forEach(person => {
-                            friendCard(person, friend)
-                        })
+    console.log(user)
+    let currentUsername
+    APIManager.getSubsetUsers(user).then(currentUser => {
+        currentUsername = currentUser[0].username
+        APIManager.getSubsetFriends(user).then(friends => {
+            console.log(friends)
+            friends.forEach(friend => {
+                const friendsToList = friend.friendID
+                console.log(friendsToList)
+                APIManager.getSubsetUsers(friendsToList).then(u => {
+                    u.forEach(person => {
+                        friendCard(person, friend)
                     })
                 })
-                addFriendButton(user)
             })
+            addFriendButton(user)
         })
-    } else {
-        console.log("getting somewhere")
-    }
+    })
 }
 
 module.exports = displayFriendList
