b66f9e6434099edec12bcfa7bafd5387031b33d8
MrErin <erinmeaker@gmail.com>
2018-05-19 22:35:43 -0500

    typo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,5 @@ const initialLoad = require("./loginPage/initialLoad")
 const displayAllData = require("./loginPage/displayAllData")
 
 
-//commenting this out to disable the login
-// initialLoad()
-
-displayAllData()
+initialLoad()
 
