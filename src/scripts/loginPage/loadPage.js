db9571f46977c5ec83ec416ed5752ae8f060a6ef
MrErin <erinmeaker@gmail.com>
2018-05-21 10:14:07 -0500

    working on styles

--- src/scripts/loginPage/loadPage.js
+++ src/scripts/loginPage/loadPage.js
@@ -2,9 +2,17 @@ const $ = require("jquery")
 const loadFriends = require("../friendList/friendList")
 const displayChat = require("../Chat/mainChat")
 
+//remove this line on master
+const displayAllData = require("./displayAllData")
+
 const loadPage = () => {
     loadFriends()
-    displayChat()
+
+    // restore commented out parts and remove indicated parts when pushing to master
+    // displayChat()
+
+    //remove this on master
+    displayAllData()
 }
 
 module.exports = loadPage
